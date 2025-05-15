3b2e3cda73355653a2f83c4f5c814042718cefd7
acah <game4lifesantuy@gmail.com>
2025-05-15 22:29:58 +0800

    anu nambah h1 doang jan lupa di apus yak

---
+++ script/script.js
@@ -0,0 +1,17 @@
+
+const navbarNav = document.querySelector('.list');
+const menu = document.querySelector('#extra')
+
+
+// ketika menu di klik
+
+menu.onclick = () => {
+    navbarNav.classList.toggle('active');
+};
+
+// close sidebar 
+
+// document.addEventListener('click', function)
+
+
+
